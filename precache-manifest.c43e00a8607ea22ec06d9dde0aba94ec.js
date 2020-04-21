a48dac0ece45787e04dec6ecaf5c924e6693ed3d
skop01 <opkm2002@hanmail.net>
2020-04-21 13:58:01 +0900

    Updates

---
+++ precache-manifest.c43e00a8607ea22ec06d9dde0aba94ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cf1f8b08cca32403bd3b8c3bdcf508e",
+    "url": "/react-page-test/index.html"
+  },
+  {
+    "revision": "85a8b184212cf2b94503",
+    "url": "/react-page-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "42e1bd24b425e3466a14",
+    "url": "/react-page-test/static/js/2.275e700f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-page-test/static/js/2.275e700f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a8b184212cf2b94503",
+    "url": "/react-page-test/static/js/main.b8441076.chunk.js"
+  },
+  {
+    "revision": "626356c64f6b9a9d0fc8",
+    "url": "/react-page-test/static/js/runtime-main.8c8129bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-page-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
